e507701469e1a2236d64207ed36a671a89a9624b
Heikki Kainulainen <hmkainul@gmail.com>
2018-11-09 09:59:49 +0100

    Clean up token recognition test

--- server/src/test/lexer.test.ts
+++ server/src/test/lexer.test.ts
@@ -2,30 +2,27 @@ import * as assert from 'assert';
 import { TokenType, lexer } from '../lexer';
 import { Range } from 'vscode-languageserver';
 
-interface TokenTypeToExample {
-    [key: number]: string;
-}
-
 describe('lexer', function () {
     it('should recognize tokens', function () {
-        let examples: TokenTypeToExample = {};
-        examples[TokenType.TAG] = '_atom_site_occupancy';
-        examples[TokenType.COMMENT] = '# This is my comment';
-        examples[TokenType.DATA] = 'data_global';
-        examples[TokenType.LOOP] = 'loop_';
-        examples[TokenType.SAVE_END] = 'save_';
-        examples[TokenType.SAVE] = 'save_foo';
-        examples[TokenType.GLOBAL] = 'global_';
-        examples[TokenType.STOP] = 'stop_';
-        examples[TokenType.SINGLE] = '\'single quoted\'';
-        examples[TokenType.DOUBLE] = '"double quoted"';
-        examples[TokenType.MULTILINE] = '\n;\nmulti\nline\n;';
-        examples[TokenType.NUMBER] = '1.23';
-        examples[TokenType.DOT] = '.';
-        examples[TokenType.QUESTION] = '?';
-        examples[TokenType.UNQUOTED] = 'unquoted';
-        examples[TokenType.WHITESPACE] = '  \t';
-        examples[TokenType.NEWLINE] = '\n';
+        let examples: { [key: number]: string } = {
+            [TokenType.TAG]: '_atom_site_occupancy',
+            [TokenType.COMMENT]: '# This is my comment',
+            [TokenType.DATA]: 'data_global',
+            [TokenType.LOOP]: 'loop_',
+            [TokenType.SAVE_END]: 'save_',
+            [TokenType.SAVE]: 'save_foo',
+            [TokenType.GLOBAL]: 'global_',
+            [TokenType.STOP]: 'stop_',
+            [TokenType.SINGLE]: '\'single quoted\'',
+            [TokenType.DOUBLE]: '"double quoted"',
+            [TokenType.MULTILINE]: '\n;\nmulti\nline\n;',
+            [TokenType.NUMBER]: '1.23',
+            [TokenType.DOT]: '.',
+            [TokenType.QUESTION]: '?',
+            [TokenType.UNQUOTED]: 'unquoted',
+            [TokenType.WHITESPACE]: '  \t',
+            [TokenType.NEWLINE]: '\n'
+        };
         for (let tokenType in examples) {
             let tokens = lexer(examples[tokenType]);
             assert.equal(tokens.length, 1);
