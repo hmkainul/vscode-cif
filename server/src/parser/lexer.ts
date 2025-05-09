fb048cc6cd71a7b9abc127ab2216f3360388d539
Heikki Kainulainen <hmkainul@gmail.com>
2025-05-09 10:19:20 +0300

    Unify tokenization

--- server/src/parser/lexer.ts
+++ server/src/parser/lexer.ts
@@ -7,9 +7,8 @@ interface LexerResult {
 }
 
 export function lexer(sourceCode: string): Token[] {
-  const expressions = sourceCode.startsWith("#\\#CIF_2.0")
-    ? cif2Expressions
-    : cif1Expressions;
+  const isCif2 = sourceCode.startsWith("#\\#CIF_2.0");
+  const expressions = selectRegexMap(isCif2);
   sourceCode = normalizeLineBreaks(sourceCode);
   const result: LexerResult = {
     tokens: [],
@@ -27,15 +26,15 @@ function normalizeLineBreaks(text: string): string {
 }
 
 function findNextToken(
-  expressions: { [key: number]: RegExp },
+  expressions: Map<TokenType, RegExp>,
   sourceCode: string,
   position: Position,
   result: LexerResult,
 ): string {
-  for (const type in expressions) {
-    const searchResult = expressions[type].exec(sourceCode);
+  for (const [tokenType, tokenRegex] of expressions) {
+    const searchResult = tokenRegex.exec(sourceCode);
     if (searchResult) {
-      switch (+type) {
+      switch (tokenType) {
         case TokenType.CIF2_TABLE_START:
           result.tableCount++;
           break;
@@ -50,14 +49,16 @@ function findNextToken(
       }
       const tokenText = searchResult[0];
       result.tokens.push({
-        type: +type,
+        type: tokenType,
         text: tokenText,
         range: tokenRange(tokenText, position),
       });
       return sourceCode.substring(tokenText.length);
     }
   }
-  throw "Lexer failed!";
+  throw new Error(
+    `Lexer failed at line ${position.line}, col ${position.character}`,
+  );
 }
 
 function tokenRange(tokenText: string, position: Position): Range {
@@ -77,50 +78,45 @@ function clone(position: Position): Position {
   return Position.create(position.line, position.character);
 }
 
-const cif1Expressions: { [key: number]: RegExp } = {
-  [TokenType.TAG]: /^_[^\s]+(?=($|\s))/,
-  [TokenType.COMMENT]: /^#.*(?=($|\n))/,
-  [TokenType.DATA]: /^DATA_[^\s]+(?=($|\s))/i,
-  [TokenType.LOOP]: /^LOOP_(?=($|\s))/i,
-  [TokenType.SAVE_END]: /^SAVE_(?=($|\s))/i,
-  [TokenType.SAVE]: /^SAVE_[^\s]+(?=($|\s))/i,
-  [TokenType.GLOBAL]: /^GLOBAL_(?=($|\s))/i,
-  [TokenType.STOP]: /^STOP_(?=($|\s))/i,
-  [TokenType.SINGLE]: /^'(?:[^']|'(?!\s|$))*'(?!\S)/,
-  [TokenType.DOUBLE]: /^"(?:[^"]|"(?!\s|$))*"(?!\S)/,
-  [TokenType.MULTILINE]: /^\n;(\n|.)*?\n;/,
-  [TokenType.NUMBER]:
-    /^([+-]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?)(\(\d+\))?(?=($|[\s]))/,
-  [TokenType.DOT]: /^(\.)(?=($|\s))/,
-  [TokenType.QUESTION]: /^(\?)(?=($|\s))/,
-  [TokenType.UNQUOTED]: /^[^\s]+/,
-  [TokenType.WHITESPACE]: /^[^\S\n]+/,
-  [TokenType.NEWLINE]: /^\n/,
-};
+type TokenRegex = RegExp | [undefined, RegExp] | [RegExp, RegExp];
 
-const cif2Expressions: { [key: number]: RegExp } = {
-  [TokenType.TAG]: /^_[^\s]+(?=($|\s))/,
-  [TokenType.COMMENT]: /^#.*(?=($|\n))/,
-  [TokenType.DATA]: /^DATA_[^\s]+(?=($|\s))/i,
-  [TokenType.LOOP]: /^LOOP_(?=($|\s))/i,
-  [TokenType.SAVE_END]: /^SAVE_(?=($|\s))/i,
-  [TokenType.SAVE]: /^SAVE_[^\s]+(?=($|\s))/i,
-  [TokenType.GLOBAL]: /^GLOBAL_(?=($|\s))/i,
-  [TokenType.STOP]: /^STOP_(?=($|\s))/i,
-  [TokenType.CIF2_TRIPLE]: /^'''(?!''').*'''/,
-  [TokenType.SINGLE]: /^'[^']*'/,
-  [TokenType.DOUBLE]: /^"[^"]*"/,
-  [TokenType.MULTILINE]: /^\n;(\n|.)*?\n;/,
-  [TokenType.NUMBER]:
+const cifExpressions = new Map<TokenType, TokenRegex>([
+  [TokenType.TAG, /^_[^\s]+(?=($|\s))/],
+  [TokenType.COMMENT, /^#.*(?=($|\n))/],
+  [TokenType.DATA, /^DATA_[^\s]+(?=($|\s))/i],
+  [TokenType.LOOP, /^LOOP_(?=($|\s))/i],
+  [TokenType.SAVE_END, /^SAVE_(?=($|\s))/i],
+  [TokenType.SAVE, /^SAVE_[^\s]+(?=($|\s))/i],
+  [TokenType.GLOBAL, /^GLOBAL_(?=($|\s))/i],
+  [TokenType.STOP, /^STOP_(?=($|\s))/i],
+  [TokenType.CIF2_TRIPLE, [undefined, /^'''(?!''').*'''/]],
+  [TokenType.SINGLE, [/^'(?:[^']|'(?!\s|$))*'(?!\S)/, /^'[^']*'/]],
+  [TokenType.DOUBLE, [/^"(?:[^"]|"(?!\s|$))*"(?!\S)/, /^"[^"]*"/]],
+  [TokenType.MULTILINE, /^\n;(\n|.)*?\n;/],
+  [
+    TokenType.NUMBER,
     /^([+-]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?)(\(\d+\))?(?=($|[\s]))/,
-  [TokenType.DOT]: /^(\.)(?=($|\s))/,
-  [TokenType.QUESTION]: /^(\?)(?=($|\s))/,
-  [TokenType.CIF2_LIST_START]: /^\[/,
-  [TokenType.CIF2_LIST_END]: /^\](?=($|\s))/i,
-  [TokenType.CIF2_TABLE_START]: /^{/,
-  [TokenType.CIF2_TABLE_DELIMINITER]: /^:/,
-  [TokenType.CIF2_TABLE_END]: /^}/,
-  [TokenType.UNQUOTED]: /^[^\s\]}]+/,
-  [TokenType.WHITESPACE]: /^[^\S\n]+/,
-  [TokenType.NEWLINE]: /^\n/,
-};
+  ],
+  [TokenType.DOT, /^(\.)(?=($|\s))/],
+  [TokenType.QUESTION, /^(\?)(?=($|\s))/],
+  [TokenType.CIF2_LIST_START, [undefined, /^\[/]],
+  [TokenType.CIF2_LIST_END, [undefined, /^\](?=($|\s))/i]],
+  [TokenType.CIF2_TABLE_START, [undefined, /^{/]],
+  [TokenType.CIF2_TABLE_DELIMINITER, [undefined, /^:/]],
+  [TokenType.CIF2_TABLE_END, [undefined, /^}/]],
+  [TokenType.UNQUOTED, [/^[^\s]+/, /^[^\s\]}]+/]],
+  [TokenType.WHITESPACE, /^[^\S\n]+/],
+  [TokenType.NEWLINE, /^\n/],
+]);
+
+function selectRegexMap(isCif2: boolean): Map<TokenType, RegExp> {
+  const result = new Map<TokenType, RegExp>();
+  for (const [tokenType, tokenRegex] of cifExpressions) {
+    const selected =
+      tokenRegex instanceof RegExp ? tokenRegex : tokenRegex[isCif2 ? 1 : 0];
+    if (selected) {
+      result.set(tokenType, selected);
+    }
+  }
+  return result;
+}
